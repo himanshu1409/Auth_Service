7dfb1d6f7e0710d8a82401efbdc6cf0cd4091f8b
Himanshu <himanshubarde2001@gmail.com>
2023-06-06 10:24:19 +0530

    Added jwt create and validate

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const apiRoutes = require("./routes/index");
 const app = express();
 
 const UserRepository = require("./repository/user-repository");
+const UserService = require("./services/user-service");
 
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
@@ -18,6 +19,17 @@ const prepareAndStartServer = () => {
     console.log(`Server started on Port: ${PORT}`);
     // const repo = new UserRepository();
     // const response = await repo.getById(1);
+    // console.log(response);
+
+    const service = new UserService();
+    // const newToken = service.createToken({
+    //   email: "himanshu@gmail.com",
+    //   id: 1,
+    // });
+    // console.log("New Token is", newToken);
+    // const token =
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhpbWFuc2h1QGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE2ODYwMjcwOTYsImV4cCI6MTY4NjAyNzEyNn0.QN-HZdVKynhof1tuQDOPl0PFm9A9FliIT1POBhf40fk";
+    // const response = service.validateToken(token);
     // console.log(response);
   });
 };
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const UserRepository = require("../repository/user-repository");
+const jwt = require("jsonwebtoken");
+
+const { JWT_KEY } = require("../config/serverConfig");
 
 class UserService {
   constructor() {
@@ -11,6 +14,26 @@ class UserService {
       return user;
     } catch (error) {
       console.log("SOmething went wrong at the Service Layer");
+      throw error;
+    }
+  }
+
+  createToken(user) {
+    try {
+      const result = jwt.sign(user, JWT_KEY, { expiresIn: "1d" });
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token verification");
+      throw error;
+    }
+  }
+
+  validateToken(token) {
+    try {
+      const response = jwt.verify(token, JWT_KEY);
+      return response;
+    } catch (error) {
+      console.log("Something went wrong in token validation", error);
       throw error;
     }
   }
